fadef1a1fa55a46396ef1685278d3f328be5275f
Harborg1 <caharborg@gmail.com>
2024-02-20 20:23:28 +0100

    added data set

--- LoadFile.py
+++ LoadFile.py
@@ -12,14 +12,11 @@ import xlrd
 # http://www.simplistix.co.uk/presentations/python-excel.pdf}
 
 # Get path to the datafile
-
-#filename = importlib_resources.files("dtuimldmtools").joinpath("data/iris.xls")
-
-filename = importlib_resources.files("dtuimldmtools").joinpath("data/Concrete_Data.xls")
+filename = importlib_resources.files("dtuimldmtools").joinpath("data/penguins.xls")
 print(filename)
 # Print the location of the iris.xls file on your computer. 
 # You should inspect it manually to understand the format and content
-print("\nLocation of the iris.xls file: {}".format(filename))
+print("\nLocation of the iris.xlsx file: {}".format(filename))
 
 
 # Load xls sheet with data
@@ -27,7 +24,7 @@ print("\nLocation of the iris.xls file: {}".format(filename))
 doc = xlrd.open_workbook(filename).sheet_by_index(0)
 
 # Extract attribute names
-attributeNames = doc.row_values(rowx=0, start_colx=0, end_colx=9)
+attributeNames = doc.row_values(rowx=0, start_colx=0, end_colx=8)
 # Try calling help(doc.row_values). You'll see that the above means
 # that we extract columns 0 through 4 from the first row of the document,
 # which contains the header of the xls files (where the attributen names are)
@@ -35,22 +32,34 @@ attributeNames = doc.row_values(rowx=0, start_colx=0, end_colx=9)
 # Extract class names to python list, then encode with integers (dict) just as
 # we did previously. The class labels are in the 5th column, in the rows 2 to
 # and up to 151:
-classLabels = doc.col_values(9, 1, 1031)  # check out help(doc.col_values)
+classLabels = doc.col_values(7, 1, 343)  # check out help(doc.col_values)
 classNames = sorted(set(classLabels))
 classDict = dict(zip(classNames, range(len(classNames))))
 
 # Extract vector y, convert to NumPy array
 y = np.array([classDict[value] for value in classLabels])
+print(y)
 
 # Preallocate memory, then extract data to matrix X
-X = np.empty((1031, 9))
-for i in range(9):
-   # print(X[:,i])
-    X[:, i] = np.array(doc.col_values(i, 1, 1031)).T
-    
+X = np.empty((336, 7))
+
+
+print("ASDSADS")
+for i in range(7):
+    X[:, i] = np.array(doc.col_values(i, 7, 343)).T
     
 
 print(X)
+
+print(X[0,0])
+
+print(X[0,1])
+print(X[0,2])
+print(X[0,3])
+print(X[0,4])
+print(X[0,5])
+print(X[0,6])
+
 
 # Compute values of N, M and C.
 N = len(y)
